1ffe2a0235dd139abadf6587c0449076955b2e6f
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-13 16:24:27 +0300

    Annabelle Mitumba Bales v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
-
+import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
     </>
