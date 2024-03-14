d2cf9307d7eaef01195971285884fb476e1215c3
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 12:33:39 +0300

    Annabelle Mitumba Bales v1

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,56 @@
+import { Fastar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ value, text }) => {
+  return (
+    <div className="rating">
+      <span>
+        {value >= 1 ? (
+          <Fastar />
+        ) : value >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 2 ? (
+          <Fastar />
+        ) : value >= 1.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 3 ? (
+          <Fastar />
+        ) : value >= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 4 ? (
+          <Fastar />
+        ) : value >= 3.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 5 ? (
+          <Fastar />
+        ) : value >= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span className="rating-text">{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
