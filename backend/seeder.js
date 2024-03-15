82d9a3e089dd1508cf0de6853f29def35bd9551c
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-15 14:50:50 +0300

    Annabelle Mitumba Bales v1

--- backend/seeder.js
+++ backend/seeder.js
@@ -20,5 +20,30 @@ const importData = async () => {
 
     const createdUsers = await User.insertMany(users);
     const adminUser = createdUsers[0]._id;
-  } catch (error) {}
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log("Data Imported!".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("Data Destroyed!".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
 };
