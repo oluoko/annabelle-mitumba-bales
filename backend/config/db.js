c416ca41f9cf5ae3822a31228ca9030b54673504
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-15 03:17:32 +0300

    Annabelle Mitumba Bales v1

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+  }
+};
+
+export default connectDB;
