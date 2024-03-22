594d638700add3632b462d0ba12038942451c47c
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-22 18:23:35 +0300

    Login Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <>
@@ -12,6 +15,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 };
