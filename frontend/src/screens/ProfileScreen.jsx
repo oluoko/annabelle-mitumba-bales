009b2c58036a170a82c1b00ead9449ab562b9f92
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-24 17:19:53 +0300

    User Profile & Update

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Table, Form, Button, Row, Col } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useProfileMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
 
 const ProfileScreen = () => {
   return <div>ProfileScreen</div>;
