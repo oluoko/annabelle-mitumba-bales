e360cc6b96834fb0a7306d5812de9e6dea83130a
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-26 02:28:19 +0300

    Multer & Image Upload Endpoint

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -3,6 +3,16 @@ import express from "express";
 import multer from "multer";
 const router = express.Router();
 
-const storage = multer.diskStorage({});
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
 export default router;
