e8559aa0267248751a0d7de7b494b3eb1b0e3867
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-25 09:33:20 +0300

    Edit Product

--- frontend/src/screens/admins/ProductEditScreen.jsx
+++ frontend/src/screens/admins/ProductEditScreen.jsx
@@ -11,6 +11,25 @@ import {
 } from "../../slices/productsApiSlice";
 
 const ProductEditScreen = () => {
+  const { id: productId } = useParams();
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const {
+    data: product,
+    error,
+    isLoading,
+    refetch,
+  } = useGetProductDetailsQuery(productId);
+
+  console.log(product);
+
   return <div>ProductEditScreen</div>;
 };
 
