4b8d833ff54d5ddf6f8a225cd5a947b6f25d6211
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-25 06:28:08 +0300

    List Products For Admin

--- frontend/src/screens/admins/ProductListScreen.jsx
+++ frontend/src/screens/admins/ProductListScreen.jsx
@@ -8,6 +8,10 @@ import { useGetProductsQuery } from "../../slices/productsApiSlice";
 const ProductListScreen = () => {
   const { data: products, error, isLoading } = useGetProductsQuery();
 
+  const deleteHandler = (id) => {
+    console.log("delete", id);
+  };
+
   return (
     <>
       <Row className="align-items-center">
@@ -15,9 +19,58 @@ const ProductListScreen = () => {
           <h1>Products</h1>
         </Col>
         <Col className="text-end">
-          <Button className="my-3">Create Product</Button>
+          <Button className="my-3">
+            <FaEdit className="mx-2" />
+            Create Product
+          </Button>
         </Col>
       </Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Table striped hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant="light" className="btn-sm mx-2">
+                        <FaEdit className="mx-2" />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm mx-2"
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <FaTrash className="mx-2" style={{ color: "white" }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
     </>
   );
 };
