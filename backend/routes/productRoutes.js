76c8f0dcb5d8a8a979767a2388ec4d40a22d43f3
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-25 09:14:35 +0300

    Edit Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,11 @@ import {
   getProductById,
   getProducts,
   createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddlware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
 
 export default router;
