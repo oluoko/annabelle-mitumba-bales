b800810e8b85396213ab86cadf12727db8032c51
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-25 06:17:26 +0300

    List Products For Admin

--- frontend/src/screens/admins/ProductListScreen.jsx
+++ frontend/src/screens/admins/ProductListScreen.jsx
@@ -6,7 +6,20 @@ import Loader from "../../components/Loader";
 import { useGetProductsQuery } from "../../slices/productsApiSlice";
 
 const ProductListScreen = () => {
-  return <div>ProductListScreen</div>;
+  const { data: products, error, isLoading } = useGetProductsQuery();
+
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className="my-3">Create Product</Button>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductListScreen;
