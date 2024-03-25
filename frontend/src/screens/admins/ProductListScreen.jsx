0228421fb74e5cc708b9e6cfaa9e17ef459effff
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-25 06:13:28 +0300

    List Products For Admin

--- frontend/src/screens/admins/ProductListScreen.jsx
+++ frontend/src/screens/admins/ProductListScreen.jsx
@@ -1,3 +1,10 @@
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { FaTimes, FaEdit, FaTrash } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { useGetProductsQuery } from "../../slices/productsApiSlice";
+
 const ProductListScreen = () => {
   return <div>ProductListScreen</div>;
 };
