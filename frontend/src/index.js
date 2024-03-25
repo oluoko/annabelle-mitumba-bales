5539b9152d5035fc291b361a96240fda64366772
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-25 09:31:22 +0300

    Edit Product

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,6 +28,7 @@ import OrderScreen from "./screens/OrderScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import OrderListScreen from "./screens/admins/OrderListScreen";
 import ProductListScreen from "./screens/admins/ProductListScreen";
+import ProductEditScreen from "./screens/admins/ProductEditScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -49,6 +50,7 @@ const router = createBrowserRouter(
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
+        <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
       </Route>
     </Route>
   )
---
+++ frontend/src/screens/admins/ProductEditScreen.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
+import { toast } from "react-toastify";
+import {
+  useUpdateProductMutation,
+  useGetProductDetailsQuery,
+} from "../../slices/productsApiSlice";
+
+const ProductEditScreen = () => {
+  return <div>ProductEditScreen</div>;
+};
+
+export default ProductEditScreen;
