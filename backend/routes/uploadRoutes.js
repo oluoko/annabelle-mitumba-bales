40e61f9cc5d378c75e2bf59246047e1980feb7e3
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-26 02:29:53 +0300

    Multer & Image Upload Endpoint

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -15,4 +15,15 @@ const storage = multer.diskStorage({
   },
 });
 
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Images only!");
+  }
+}
+
 export default router;
