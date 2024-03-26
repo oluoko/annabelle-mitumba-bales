cef1f9d0db7bf4fcbdb0a8029c3e25b87427e74d
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-26 10:17:01 +0300

    Delete Products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,10 +5,15 @@ import {
   getProducts,
   createProduct,
   updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddlware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
