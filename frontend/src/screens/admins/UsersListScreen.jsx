98901724478a3a33ffc9d606d767b78b69296166
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-27 08:24:35 +0300

    List Users For Admin

--- frontend/src/screens/admins/UsersListScreen.jsx
+++ frontend/src/screens/admins/UsersListScreen.jsx
@@ -20,7 +20,7 @@ const UsersListScreen = () => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Table stried hover responsive className="table-sm">
+        <Table striped hover responsive className="table-sm">
           <thead>
             <tr>
               <th>ID</th>
