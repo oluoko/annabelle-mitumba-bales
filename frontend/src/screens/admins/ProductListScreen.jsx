48cfe436d93845f2e0c802f30fdfdb61ae952e5f
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-28 03:45:56 +0300

    Paginate Component

--- frontend/src/screens/admins/ProductListScreen.jsx
+++ frontend/src/screens/admins/ProductListScreen.jsx
@@ -1,4 +1,5 @@
 import { LinkContainer } from "react-router-bootstrap";
+import { useParams } from "react-router-dom";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import Message from "../../components/Message";
@@ -11,7 +12,11 @@ import {
 } from "../../slices/productsApiSlice";
 
 const ProductListScreen = () => {
-  const { data: products, error, isLoading, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, error, isLoading, refetch } = useGetProductsQuery({
+    pageNumber,
+  });
 
   const [createProduct, { isLoading: loadingCreate }] =
     useCreateProductMutation();
@@ -76,7 +81,7 @@ const ProductListScreen = () => {
               </tr>
             </thead>
             <tbody>
-              {products.map((product) => (
+              {data.products.map((product) => (
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
