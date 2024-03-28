2d302da1ca737552a071cbfb380c25a6bf17d2d0
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-28 04:10:47 +0300

    Search Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -33,7 +33,11 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
-          <Paginate pages={data.pages} page={data.page} />
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            keyword={keyword ? keyword : ""}
+          />
         </>
       )}
     </>
