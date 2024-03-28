6787eadb5d4d8f6e716d1110afaa5975068b6678
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-28 12:54:34 +0300

    Product Carousel - backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,10 +7,12 @@ import {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddlware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
